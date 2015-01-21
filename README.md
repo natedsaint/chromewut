36695c5591e4564e9393f2a77ec19393b5dcb9e1
Nathan St. Pierre <nsp@amazon.com>
2015-01-21 12:30:56 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+##wut.link extension for chrome##
+
+Requires developer mode: just choose "developer mode" from the extension menu, then choose "load unpacked extension" and point it to the root folder. Done.
---
+++ popup.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $("#generateLink").on("click",generateLink);
+  $("#link").on("click",selectAll);
+});
+
+function selectAll() {
+  $("#link").focus();
+  $("#link").select();
+}
+function generateLink() {
+  chrome.tabs.query({'active': true,'currentWindow':true}, function(tabs) {
+    var activeTab = tabs[0],
+        url = activeTab.url;
+    $.ajax({
+      url:"http://www.wut.link/links",
+      type:"POST",
+      dataType: "json",
+      data:{
+        url:url
+      },
+      success: function(response,data) {
+          $("#link").html(response.proxyUrl);
+          $("#link").addClass("loaded");
+      }
+    });
+  });
+}
