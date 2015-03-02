2056ced51cd2fec024b299f2c4c9fa9108c6348b
Nathan St. Pierre <natedsaint@gmail.com>
2015-03-02 14:37:17 -0800

    Adding start/end date, refactoring js logic

--- popup.js
+++ popup.js
@@ -1,42 +1,82 @@
-$(document).ready(function() {
-  $("#generateLink").on("click",generateLink);
-  $("#link").on("click",selectAll);
-  $("#copyButton").on("click",executeCopy);
-});
+/* globals jQuery, controller, chrome */
+"use strict";
 
-function selectAll() {
-  $("#link").focus();
-  $("#link").select();
-}
-function executeCopy() {
-    var input = document.createElement('textarea');
-    document.body.appendChild(input);
-    input.value = $("#link").html();
-    input.focus();
-    input.select();
-    document.execCommand('Copy');
-    input.remove();
-    $(".linkHolder").addClass("copied");
-    setTimeout(function() {
-      $(".linkHolder").removeClass("copied");
-    },2000);
-}
+var WutController = function() {
 
-function generateLink() {
-  chrome.tabs.query({'active': true,'currentWindow':true}, function(tabs) {
-    var activeTab = tabs[0],
-        url = activeTab.url;
-    $.ajax({
-      url:"http://wut.link/",
-      type:"POST",
-      dataType: "json",
-      data:{
-        url:url
-      },
-      success: function(response,data) {
-          $("#link").html(response.proxyUrl);
-          $(".linkHolder").addClass("loaded");
-      }
+};
+
+WutController.prototype = {
+    pad: function(n){
+        return n<10 ? '0'+n : n
+    },
+    selectAll: function() {
+        $("#link").focus();
+        $("#link").select();
+    },
+    executeCopy: function() {
+        var input = document.createElement('textarea');
+        document.body.appendChild(input);
+        input.value = $("#link").html();
+        input.focus();
+        input.select();
+        document.execCommand('Copy');
+        input.remove();
+        $(".linkHolder").addClass("copied");
+        setTimeout(function() {
+            $(".linkHolder").removeClass("copied");
+        },2000);
+    },
+    generateLink : function() {
+        var url = $("#urlField").val() || $("#urlField").attr("placeholder"),
+            activateDate = $("#activationDateField").val(),
+            deactivateDate = $("#deactivationDateField").val(),
+            button = $("#generateLink");
+
+        if (button.hasClass("disabled")) {
+            return false;
+        }
+
+        button.addClass("disabled").html("Generating...");
+
+        $.ajax({
+            url:"http://wut.link/",
+            type:"POST",
+            dataType: "json",
+            data:{
+                url:url,
+                activateDate: activateDate,
+                deactivateDate: deactivateDate
+            },
+            success: function(response,data) {
+                $("#link").html(response.proxyUrl);
+                $(".linkHolder").addClass("loaded");
+                button.removeClass("disabled").html("Create Link");
+            }
+        });
+    },
+    bindListeners: function() {
+        // set placeholder to current url (will use this if not provided by user)
+        chrome.tabs.query({'active': true,'currentWindow':true}, function(tabs) {
+            $("#urlField").attr("placeholder",tabs[0].url);
+        });
+
+        $("#generateLink").on("click",this.generateLink);
+        $("#link").on("click",this.selectAll);
+        $("#copyButton").on("click",this.executeCopy);
+    }
+};
+
+var wut = new WutController();
+
+
+(function($,wut) {
+    $(document).ready(function() {
+        wut.bindListeners();
     });
-  });
-}
+})(jQuery,wut);
+
+
+
+
+
+
